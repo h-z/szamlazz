fc5c2429789ada52a400fee0bcef44eff177281d
Zoltan Hegedus <hz@muszaki.info>
2023-01-28 23:06:44 +0100

    Initial commit

---
+++ lib/szamlazz.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "szamlazz/version"
+
+module Szamlazz
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/szamlazz/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Szamlazz
+  VERSION = "0.1.0"
+end
