f160a03c056b062de418704deaf6c5cc84044a49
Zoltan Hegedus <hz@muszaki.info>
2023-01-29 18:06:09 +0100

    introduce base value classes

--- lib/szamlazz.rb
+++ lib/szamlazz.rb
@@ -4,5 +4,27 @@ require_relative "szamlazz/version"
 
 module Szamlazz
   class Error < StandardError; end
-  # Your code goes here...
+
+  class Base
+    def initialize(params)
+      @params = params
+      params.each { |key, value| __send__("#{key}=", value) }
+    end
+  end
+
+  class Address < Base
+    attr_accessor :name, :country, :zip, :city, :address
+  end
+
+  class Seller < Base
+    attr_accessor :bank_name, :bank_account, :email_address, :email_subject, :email_message
+  end
+
+  class Buyer < Base
+    attr_accessor :address, :post_address, :tax_number, :issuer_name, :identifier, :phone, :comment
+  end
+
+  class Item < Base
+    attr_accessor :label, :quantity, :unit, :vat, :net_unit_price, :gross_unit_price, :comment
+  end
 end
